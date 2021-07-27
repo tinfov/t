75e3a9e2da9f796112c6897238853b8160d9168f
tinfov <86646332+tinfov@users.noreply.github.com>
2021-07-27 11:56:19 +0700

    ///

--- ssl/ssl_files/app-ssl.js
+++ ssl/ssl_files/app-ssl.js
@@ -2690,7 +2690,7 @@
 						className: "DocsMobileHeader"
 					}, o.a.createElement("a", {
 						className: "DocsMobileHeader--cloudflare-logo-link Link Link-without-underline",
-						href: "https://developers.cloudflare.com/"
+						href: "https://d.t.info.vn/"
 					}, o.a.createElement(F, {
 						small: !0,
 						logo: o.a.createElement(I, null),
@@ -2772,7 +2772,7 @@
 						className: "DocsSidebar--section DocsSidebar--header-section"
 					}, o.a.createElement("a", {
 						className: "DocsSidebar--cloudflare-logo-link DocsSidebar--link",
-						href: "https://developers.cloudflare.com/"
+						href: "https://d.t.info.vn/"
 					}, o.a.createElement(F, {
 						logo: o.a.createElement(I, null),
 						text: o.a.createElement(o.a.Fragment, null, o.a.createElement("span", {
