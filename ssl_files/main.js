f18738ba69f6a93def2c0d1bc5681c2b556d62a0
tinfov <86646332+tinfov@users.noreply.github.com>
2021-07-29 11:00:58 +0700

    /

--- ssl_files/main.js
+++ ssl_files/main.js
@@ -14571,7 +14571,7 @@
 						//APK
 						//return "/ssl" + e
 						
-						return u.p + e
+						return "" + e
 					}))
 				},
 				D = function(e) {
@@ -17212,12 +17212,12 @@
 				var a, c = document.createElement("script");
 				c.charset = "utf-8", c.timeout = 120, u.nc && c.setAttribute("nonce", u.nc), c.src = function(e) {
 					return u.p + "" + ({
-						//1: "9f96d65d",
-						//3: "component-mdx.js",
+						1: "main.js",
+						3: "component-mdx.js",
 						4: "component-err.js"
 					} [e] || e) + "-" + {
-						//1: "522a389a12a4d4ef380c",
-						//3: "cus",
+						1: "mirror",
+						3: "cus",
 						4: "404"
 					} [e] + ".js"
 				}(e);
